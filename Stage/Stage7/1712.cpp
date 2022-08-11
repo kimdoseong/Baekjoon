c87bff45278eba341854bac28d05d8d2aa92ad95
kimdoseong <spl8714@gmail.com>
2022-08-11 16:30:44 +0900

    Add Stage 7

---
+++ Stage/Stage7/1712.cpp
@@ -0,0 +1,16 @@
+//
+// Created by doseong on 2022-08-11.
+//
+
+/**
+ * Baekjoon #1712 - 손익분기점
+ * Bronze 2
+ */
+
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+}
