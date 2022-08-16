02c0f41c7bb75c0b3e5fbeacbd45b79353ce836a
kimdoseong <spl8714@gmail.com>
2022-08-17 00:33:18 +0900

    #1978

--- Stage/Stage8/1978.cpp
+++ Stage/Stage8/1978.cpp
@@ -9,7 +9,41 @@
  */
 
 #include <iostream>
+#include <cmath>
+
+bool is_prime(int num) {
+    if (num > 2) {
+        if (num % 2 == 0)
+            return false;
+
+        auto sqrt_num = static_cast<long>(std::sqrt(static_cast<double>(num)));
+        for (long j = 3; j <= sqrt_num; j += 2) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+    } else if (num == 1 || num == 0)
+        return false;
+
+    return true;
+}
 
 int main() {
-    
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int num;
+        std::cin >> num;
+
+        if (is_prime(num))
+            cnt++;
+    }
+
+    std::cout << cnt;
+    return 0;
 }
