7564abd479b328a0c2ced5a2965f5c50fefeced5
kimdoseong <spl8714@gmail.com>
2022-08-19 13:16:06 +0900

    #2798

--- Stage/Stage10/2798.cpp
+++ Stage/Stage10/2798.cpp
@@ -8,7 +8,33 @@
  */
 
 #include <iostream>
+#include <vector>
 
-int main(){
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    std::vector<int> number;
+    for (int i = 0; i < n; i++) {
+        int num;
+        std::cin >> num;
+        number.emplace_back(num);
+    }
+
+    int result = 0;
+    for (int i = 0; i < number.size(); i++) {
+        for (int j = i + 1; j < number.size(); j++) {
+            for (int k = j + 1; k < number.size(); k++) {
+                auto tmp = number[i] + number[j] + number[k];
+
+                if (tmp > result && tmp <= m) {
+                    result = tmp;
+                }
+
+            }
+        }
+    }
+
+    std::cout << result;
     return 0;
 }
