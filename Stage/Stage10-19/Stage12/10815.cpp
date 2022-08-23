eb07d202d0dac6b4305c6948b96324d5c2e749c0
kimdoseong <spl8714@gmail.com>
2022-08-23 13:33:50 +0900

    Add Stage 12

---
+++ Stage/Stage10-19/Stage12/10815.cpp
@@ -0,0 +1,18 @@
+//
+// Created by doseong on 2022-08-23.
+//
+
+/**
+ * Baekjoon #10815 - 숫자 카드
+ * Silver 5
+ */
+
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cout << "Hello World" << "\n";
+    return 0;
+}
