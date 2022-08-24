58dda63907148e96eb70df4a614bc0e5e2c84a68
kimdoseong <spl8714@gmail.com>
2022-08-24 23:06:48 +0900

    #10815

--- Stage/Stage10-19/Stage12/10815.cpp
+++ Stage/Stage10-19/Stage12/10815.cpp
@@ -8,11 +8,52 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::cout << "Hello World" << "\n";
+    int n;
+    std::cin >> n;
+    std::vector<int> nV(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> nV[i];
+    }
+    std::sort(nV.begin(), nV.end());
+
+    int m;
+    std::cin >> m;
+    std::vector<int> mV(m);
+    for (int i = 0; i < m; i++) {
+        std::cin >> mV[i];
+    }
+
+    std::vector<int> out(m, 0);
+    for (int i = 0; i < mV.size(); i++) {
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nV[mid] == mV[i]) {
+                out[i] = 1;
+                break;
+            }
+
+            if (nV[mid] > mV[i]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+
+    std::for_each(out.cbegin(), out.cend(), [](const int n) {
+        std::cout << n << " ";
+    });
+
     return 0;
 }
