ffa628d0662f78ead95954e79f6c1c0424c9409b
kimdoseong <spl8714@gmail.com>
2022-08-26 00:24:50 +0900

    Add Stage 13

---
+++ Stage/Stage10-19/Stage13/1085.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kimdoseong on 2022/08/26.
+//
+
+/**
+ * Baekjoon #1085 - 직사각형에서 탈출
+ * Bronze 3
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
