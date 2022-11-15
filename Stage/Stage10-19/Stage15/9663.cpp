f5c7fa960bbe998373e1a1d3a0ce0e16ad1bea06
kimdoseong <spl8714@gmail.com>
2022-11-15 23:59:23 +0900

    #9663 NQueen

--- Stage/Stage10-19/Stage15/9663.cpp
+++ Stage/Stage10-19/Stage15/9663.cpp
@@ -7,10 +7,36 @@
  * Gold 4
  */
 
+// X-A = Y-B
+
+
 #include <iostream>
+#include <array>
+
+int n, count = 0;
+std::array<int, 15> arr{0,};
+
+bool Check(int level) {
+    for (int i = 0; i < level; i++) {
+        if (arr[i] == arr[level] || abs(arr[level] - arr[i]) == level - i) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 void NQueen(int x) {
-
+    if (x == n) {
+        count++;
+    } else {
+        for (int i = 0; i < n; i++) {
+            arr[x] = i;
+            if (Check(x)) {
+                NQueen(x + 1);
+            }
+        }
+    }
 }
 
 
@@ -18,9 +44,9 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int n;
     std::cin >> n;
 
     NQueen(0);
+    std::cout << count;
     return 0;
 }
