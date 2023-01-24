fcc2efdc7850f925c77812254d2e341d050f4026
kimdoseong <spl8714@gmail.com>
2023-01-24 22:07:02 +0900

    Init Stage 16

---
+++ Stage/Stage10-19/Stage16/24416.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kimdoseong on 2023/01/24.
+//
+
+/**
+ * Baekjoon #24416 - 알고리즘 수업 - 피보나치 수 1
+ * Bronze 1
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
