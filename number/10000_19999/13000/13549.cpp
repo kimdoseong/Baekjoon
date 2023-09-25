2a5deb302696791ab29eb6a63783c78aeb65d750
kimdoseong <spl8714@gmail.com>
2023-09-25 17:18:32 +0900

    Add 13000 directory

---
+++ number/10000_19999/13000/13549.cpp
@@ -0,0 +1,18 @@
+//
+// Created by kimdoseong on 2023/09/25.
+//
+
+/**
+ * Baekjoon #13549 - 숨바꼭질 3
+ * Gold 5
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    return 0;
+}
